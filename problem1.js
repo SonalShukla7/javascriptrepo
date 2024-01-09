a8d1fa1cdb393d72965777153759374a034248b8
SonalShukla7 <sonalshukla358jnp@gmail.com>
2024-01-09 08:23:52 +0530

    evenodd

---
+++ problem1.js
@@ -0,0 +1,11 @@
+//if i want ti check a number is odd or even
+
+//if the reminder is 0 we can say that is even
+
+let number=8
+if(number%2==0){
+console.log("even");
+}
+else{
+console.log("Odd");
+}
